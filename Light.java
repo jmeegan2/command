fd689f4482d6641659deeb53727810467a7a35b5
jmeegan2 <jmeegan8@gmail.com>
2022-11-08 18:15:59 -0500

    Initial commit

---
+++ Light.java
@@ -0,0 +1,11 @@
+public class Light {
+    private boolean on;
+    public void switchOn(){
+        System.out.println("Lights on");
+        on = true;
+    }
+    public void switchOff(){
+        System.out.println("Lights off");
+        on = false;
+    }
+}
---
+++ LightOnCommand.java
@@ -0,0 +1,10 @@
+public class LightOnCommand implements Command{
+    //reference to the light
+    Light light;
+    public LightOnCommand(Light light){
+        this.light = light;
+    }
+    public void execute(){
+        light.switchOn();
+    }
+}
